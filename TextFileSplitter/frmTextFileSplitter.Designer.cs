95741aac9e8e523e8dc05a067ab788ed94b8a8f2
Emmanuel Medina Lopez <ing.emmanuelmedina@gmail.com>
2017-08-07 15:30:41 -0600

    Add files via upload

---
+++ TextFileSplitter/frmTextFileSplitter.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace TextFileSplitter
+{
+    partial class frmTextFileSplitter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPrefix = new System.Windows.Forms.TextBox();
+            this.txtSuffix = new System.Windows.Forms.TextBox();
+            this.nudMaxLines = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSplit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMaxLines)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(498, 8);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File to Split: ";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Enabled = false;
+            this.txtFilePath.Location = new System.Drawing.Point(83, 10);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.ReadOnly = true;
+            this.txtFilePath.Size = new System.Drawing.Size(409, 20);
+            this.txtFilePath.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Prefix: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(271, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Suffix: ";
+            // 
+            // txtPrefix
+            // 
+            this.txtPrefix.Location = new System.Drawing.Point(83, 40);
+            this.txtPrefix.Name = "txtPrefix";
+            this.txtPrefix.Size = new System.Drawing.Size(173, 20);
+            this.txtPrefix.TabIndex = 5;
+            // 
+            // txtSuffix
+            // 
+            this.txtSuffix.Location = new System.Drawing.Point(319, 40);
+            this.txtSuffix.Name = "txtSuffix";
+            this.txtSuffix.Size = new System.Drawing.Size(173, 20);
+            this.txtSuffix.TabIndex = 6;
+            // 
+            // nudMaxLines
+            // 
+            this.nudMaxLines.Increment = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudMaxLines.Location = new System.Drawing.Point(83, 74);
+            this.nudMaxLines.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.nudMaxLines.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudMaxLines.Name = "nudMaxLines";
+            this.nudMaxLines.Size = new System.Drawing.Size(120, 20);
+            this.nudMaxLines.TabIndex = 7;
+            this.nudMaxLines.Value = new decimal(new int[] {
+            500000,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Max Lines: ";
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.Location = new System.Drawing.Point(16, 109);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(557, 23);
+            this.btnSplit.TabIndex = 9;
+            this.btnSplit.Text = "Split the File";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+            // 
+            // frmTextFileSplitter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 149);
+            this.Controls.Add(this.btnSplit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nudMaxLines);
+            this.Controls.Add(this.txtSuffix);
+            this.Controls.Add(this.txtPrefix);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "frmTextFileSplitter";
+            this.Text = "Text File Splitter";
+            ((System.ComponentModel.ISupportInitialize)(this.nudMaxLines)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPrefix;
+        private System.Windows.Forms.TextBox txtSuffix;
+        private System.Windows.Forms.NumericUpDown nudMaxLines;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSplit;
+    }
+}
+
---
+++ TextFileSplitter/frmTextFileSplitter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TextFileSplitter
+{
+    public partial class frmTextFileSplitter : Form
+    {
+        private string path;
+
+        public frmTextFileSplitter()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {                
+                path = ofd.FileName;
+                txtFilePath.Text = path;
+            }
+        }
+
+        private void btnSplit_Click(object sender, EventArgs e)
+        {
+            var lines = File.ReadAllLines(path);
+            int maxLines = (int)nudMaxLines.Value;
+            string[] fileLines = new string[maxLines];
+
+            string prefix = txtPrefix.Text;
+            string suffix = txtSuffix.Text;
+
+            int lastPeriod = path.LastIndexOf(".");
+            string extension = path.Substring(lastPeriod + 1, path.Length - 1 - lastPeriod);
+            string oldFilePath = path.Substring(0, path.LastIndexOf("\\") + 1);
+
+            int count = 0;
+
+            for (int i = 0, j = 0; i < lines.Length; i++, j++)
+            {
+                if (j == maxLines || i == lines.Length - 1)
+                {
+                    j = 0;
+                    count++;
+                    
+                    string newFilePath = string.Format("{0}{1}{2}{3}.{4}", oldFilePath, prefix, count, suffix, extension);
+
+                    File.WriteAllLines(newFilePath, fileLines);
+                    fileLines = new string[maxLines];
+                }
+
+                fileLines[j] = lines[i];
+            }            
+
+            lines = null;
+            fileLines = null;
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+
+            MessageBox.Show(string.Format("Finished splitting, created {0} files in {1}", count + 1, oldFilePath), "Finished", MessageBoxButtons.OK);
+        }
+    }
+}
